145c5a84897be71114af73c29ea64e97f37f6895
Deepmalya Acharya <67915759+Deepmalya1@users.noreply.github.com>
2025-08-02 13:21:52 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-echo "🧠 Building BrainWorkshop with Nuitka..."
-
-# Install Nuitka if not installed
-pip install --upgrade nuitka
-
-# Build the .exe with Nuitka (one line)
-nuitka --standalone --onefile --windows-disable-console brainworkshop.py
